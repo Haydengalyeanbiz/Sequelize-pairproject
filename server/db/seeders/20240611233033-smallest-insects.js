3b7a4ea820bfcdc2449952dfc9516c23b1958e8d
Hayden <haydengalyeanbiz@gmail.com>
2024-06-11 19:39:51 -0400

    finished project

---
+++ server/db/seeders/20240611233033-smallest-insects.js
@@ -0,0 +1,31 @@
+'use strict';
+const { Insect } = require("../models")
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await Insect.bulkCreate([
+      {
+        name: "Western Pygmy Blue Butterfly",
+        fact:"Thinks Dr.Martin Luther King Killed Himself",
+        millimeters: 12,
+      }
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
